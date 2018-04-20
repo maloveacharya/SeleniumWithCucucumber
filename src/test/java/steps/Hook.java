16bc137a0e9e6478ba21b631a26128fe05fdeb5a
maloveacharya <30867112+maloveacharya@users.noreply.github.com>
2018-04-20 10:52:20 -0400

    change driver

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -25,12 +25,12 @@ public class Hook extends BaseUtil{
         System.out.println("Opening the browser : Firefox");
 
         System.setProperty("webdriver.firefox.marionette", "D:\\Libs\\geckodriver.exe");
-        base.Driver = new FirefoxDriver();*/
+        base.Driver = new FirefoxDriver();
 
 
         //Chrome driver
         /*System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
-        base.Driver = new ChromeDriver();
+        base.Driver = new ChromeDriver();*/
     }
 
 
